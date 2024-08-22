54be3f0c6305d9a98c3ec9f2dcd8dd3c6d3733a3
Laflorentie Nicolas <laflorentie.nicolas@gmail.com>
2024-08-22 12:04:17 +0200

    Add carousel counter, change className for BEM

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -16,15 +16,18 @@ const Carousel = ({ images }) => {
 
   return (
     <div className="carousel">
-      <button className="carousel-control prev" onClick={goToPrev}>
+      <button className="carousel__control prev" onClick={goToPrev}>
         ‹
       </button>
-      <div className="carousel-slides">
+      <div className="carousel__slides">
         <img src={images[currentIndex]} alt={`Slide ${currentIndex + 1}`} />
       </div>
-      <button className="carousel-control next" onClick={goToNext}>
+      <button className="carousel__control next" onClick={goToNext}>
         ›
       </button>
+      <div className="carousel__counter">
+        {currentIndex + 1} / {images.length}
+      </div>
     </div>
   );
 };
