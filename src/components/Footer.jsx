f657f71997c4969e451d8723d5d3f1abf447c3a4
Laflorentie Nicolas <laflorentie.nicolas@gmail.com>
2024-08-22 18:13:08 +0200

    Change footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,29 +8,46 @@ function Footer() {
   return (
     <footer>
       <section id="contact" className="footer">
-        <a
-          href="https://www.linkedin.com/in/nicolas-laflorentie-b1880927/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FontAwesomeIcon
-            icon={faLinkedin}
-            aria-label="LinkedIn"
-            className="logo"
-          />
-        </a>
-
-        <a href="mailto:laflorentie.nicolas@gmail.com">
-          <FontAwesomeIcon
-            icon={faEnvelope}
-            aria-label="Email"
-            className="logo"
-          />
-        </a>
-
-        <a href="tel:0685605988">
-          <FontAwesomeIcon icon={faPhone} aria-label="Phone" className="logo" />
-        </a>
+        <h2 className="footer__title">Me contacter</h2>
+        <div className="footer__contact">
+          <div className="footer__contact-item">
+            <FontAwesomeIcon
+              icon={faPhone}
+              aria-label="Phone"
+              className="logo"
+            />
+            <a href="tel:0685605988" className="footer__contact-link">
+              06 85 60 59 88
+            </a>
+          </div>
+          <div className="footer__contact-item">
+            <FontAwesomeIcon
+              icon={faEnvelope}
+              aria-label="Email"
+              className="logo"
+            />
+            <a
+              href="mailto:laflorentie.nicolas@gmail.com"
+              className="footer__contact-link"
+            >
+              laflorentie.nicolas@gmail.com
+            </a>
+          </div>
+        </div>
+        <div className="footer__social">
+          <a
+            href="https://www.linkedin.com/in/nicolas-laflorentie-b1880927/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="footer__social-link"
+          >
+            <FontAwesomeIcon
+              icon={faLinkedin}
+              aria-label="LinkedIn"
+              className="logo"
+            />
+          </a>
+        </div>
       </section>
     </footer>
   );
